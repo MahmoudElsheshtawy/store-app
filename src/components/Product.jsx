372ea121ac7aaf2d07c4704df8923714dd8494db
mahmoud <MahmoudElsheshtawy@users.noreply.github.com>
2024-07-12 18:00:47 +0300

    pro

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,6 +1,31 @@
+// import { Link } from "react-router-dom";
+// import {BsPlus ,BsEyeFill} from 'react-icons/bs'
 
-const Product = () => {
-  return <div>Product</div>;
+
+
+
+// eslint-disable-next-line react/prop-types
+const Product = ({product}) => {
+
+  console.log(product);
+  const {id , image ,category,title, price} = product;
+  return (
+    <>
+    <div className=" border  ">
+1
+    </div>
+    <div>2</div>
+
+    
+    
+    
+    
+    
+    
+    
+    
+    </>
+  )
 };
 
 export default Product;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,16 +1,27 @@
 import { useContext } from "react";
 import { ProductContext } from "../contexts/ProductContext";
+import Product from "../components/Product";
 const Home = () => {
   const { products } = useContext(ProductContext);
 
   console.log(products);
 
   return (
-    <div>
-      <section>
-        <div className="container mx-auto"></div>
+    <>
+      <section className="py-16">
+        <div className="container mx-auto xl:px-[90px]">
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 xl:grid-cols-5 gap-[30px] max-w-sm
+          mx-auto md:max-w-none md:mx-0
+          ">
+            {products.map((product) => {
+              return (
+              <Product product={product} key={product.id}/>
+              );
+            })}
+          </div>
+        </div>
       </section>
-    </div>
+    </>
   );
 };
 
