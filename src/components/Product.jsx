b58a5fb574b36dfd37cdbf822a65ed95117bed7c
mahmoud <MahmoudElsheshtawy@users.noreply.github.com>
2024-07-13 18:46:48 +0300

    add and vewi

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,31 +1,45 @@
 // import { Link } from "react-router-dom";
-// import {BsPlus ,BsEyeFill} from 'react-icons/bs'
-
-
-
+import { BsPlus, BsEyeFill } from "react-icons/bs";
+import { Link } from "react-router-dom";
 
 // eslint-disable-next-line react/prop-types
-const Product = ({product}) => {
-
+const Product = ({ product }) => {
   console.log(product);
-  const {id , image ,category,title, price} = product;
+  // eslint-disable-next-line react/prop-types
+  const { id, image, category, title, price } = product;
   return (
     <>
-    <div className=" border  ">
-1
+    <div>
+    <div className="  border border-[#e4e4e4] h-[300px] mb-4 relative overflow-hidden group transition flex flex-col">
+        {/* img */}
+        <div className=" w-full h-full flex justify-center ">
+          <div className=" w-[200px] mx-auto flex justify-center items-center">
+            <img
+              className="max-h-[160px] group-hover:scale-110 transition duration-300 cursor-pointer"
+              src={image}
+              alt="product"
+            />
+          </div>
+        </div>
+        {/* button */}
+        <div className="absolute top-6 -right-11 group-hover:right-4 p-2  flex flex-col items-center justify-center gap-y-2 opacity-0 group-hover:opacity-100 transition-all ">
+          <button>
+            <div  className="flex justify-center items-center w-12 h-12 text-white bg-slate-600">
+              <BsPlus className=" text-2xl" />
+            </div>
+          </button>
+          <Link to={`/product/${id}`} className="w-12 h-12 bg-white flex justify-center items-center text-primary drop-shadow-xl">
+          <BsEyeFill/>
+          </Link>
+        </div>
+      </div>
+      <div>
+        {category}
+      </div>
     </div>
-    <div>2</div>
-
-    
-    
-    
-    
-    
-    
-    
-    
+  
     </>
-  )
+  );
 };
 
 export default Product;
