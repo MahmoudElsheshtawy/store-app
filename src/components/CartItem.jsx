9f14cdd51178870081d75ef7c99cd6fb6023f5e2
mahmoud <MahmoudElsheshtawy@users.noreply.github.com>
2024-07-17 23:49:00 +0300

    rr

--- src/components/CartItem.jsx
+++ src/components/CartItem.jsx
@@ -1,17 +1,18 @@
+import { IoMdClose } from "react-icons/io";
 import { Link } from "react-router-dom";
 
 // eslint-disable-next-line react/prop-types
 const CartItem = ({ item }) => {
   const { id, title, price, amount, image } = item;
   return (
-    <div className=" flex ">
+    <div className=" flex bg-slate-400 ">
       {/* img */}
-      <div className="w-full min-h-[150px] flex items-center gap-x-4">
+      <div className=" min-h-[150px] flex items-center bg-slate-500 gap-x-4">
         <Link to={`/product/${id}`}>
           <img className="max-w-[80px]" src={image} alt="" />
         </Link>
-      </div>
-      <div>
+        <div>
+        <div className="mb-2 flex justify-between items-center">
         <Link
           to={`/product/${id}`}
           className=" text-sm uppercase font-medium max-w-[240px]
@@ -19,7 +20,15 @@ const CartItem = ({ item }) => {
         >
           {title}
         </Link>
+        <div>
+          <IoMdClose className=" cursor-pointer text-gray-500 hover:text-red-500 transition"/>
+        </div>
       </div>
+
+
+        </div>
+      </div>
+     
     </div>
   );
 };
