10df12b9aa4355b01799a1150e0e4e950e28ba17
mahmoud <MahmoudElsheshtawy@users.noreply.github.com>
2024-08-04 12:57:13 +0300

    handle footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,15 @@
-import React from 'react';
+
 
 const Footer = () => {
-  return <div>Foojjijiiter</div>;
+  return <footer className=" bg-primary py-6">
+    <div className=" container mx-auto flex justify-center ">
+      <p className="text-white ">
+      Created by Mahmoud_Elsheshtawy.
+      </p>
+    </div>
+
+
+  </footer>
 };
 
 export default Footer;
